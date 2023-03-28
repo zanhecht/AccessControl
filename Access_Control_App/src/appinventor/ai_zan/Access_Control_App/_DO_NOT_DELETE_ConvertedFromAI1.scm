5ed06307b7a329e0c5180f408080c0a04e3110e3
zanhecht <797985+zanhecht@users.noreply.github.com>
2023-03-28 01:01:51 -0400

    Move keypad below output

--- Access_Control_App/src/appinventor/ai_zan/Access_Control_App/_DO_NOT_DELETE_ConvertedFromAI1.scm
+++ Access_Control_App/src/appinventor/ai_zan/Access_Control_App/_DO_NOT_DELETE_ConvertedFromAI1.scm
@@ -1,4 +1,4 @@
 #|
 $JSON
-{"YaVersion":"172","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"23","Uuid":"0","Title":"Screen1","$Components":[]}}
+{"YaVersion":"221","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"30","Uuid":"0","Title":"Screen1","$Components":[],"Sizing":"Fixed","ShowListsAsJson":"False"}}
 |#
